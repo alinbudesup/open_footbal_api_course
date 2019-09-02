874adc00ba43b07573f03a1cabb56e99ec5f8ab2
alin budes <alin.budes@unifiedpost.com>
2019-09-02 08:44:50 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require 'sidekiq/web'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
+  resources :teams do
+    get :download_logo, on: :member
+  end
+
+  resources :managers, only: %i[index show create update]
+end
